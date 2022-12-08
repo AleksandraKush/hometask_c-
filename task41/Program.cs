803d98d91ec31233f44c7ea98bc84c90afa0dc7e
александра <aleksandra@MacBook-Pro-aleksandra.local>
2022-12-08 20:37:48 +0300

    task41

---
+++ task41/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.WriteLine("введите количество чисел, которое собираетесь вводить");
+int size = Convert.ToInt32(Console.ReadLine());
+
+int[] NewArray(int num)
+{
+    int[] array = new int[num];
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.WriteLine("введите число");
+        array[i] = (Convert.ToInt32(Console.ReadLine()));
+    }
+    return array;
+}
+
+int FindResult(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+int[] massive = NewArray(size);
+Console.WriteLine($"Количество чисел, которые больше 0, {FindResult(massive)}");
