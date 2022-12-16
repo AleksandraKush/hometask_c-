8d8803c7bc6d33d45a5a3ca867e7f8012cc406ee
александра <aleksandra@MacBook-Pro-aleksandra.local>
2022-12-16 18:59:03 +0300

    add task66

---
+++ task66/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("введите число N");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("введите число M");
+int m = Convert.ToInt32(Console.ReadLine());
+
+int result = Natural(n, m);
+Console.Write(result);
+
+int Natural(int num1, int num2)
+{
+    if(num1 > num2)
+    {
+        num1 += Natural(num1 - 1, num2);
+    }
+    else if(num1 < num2) 
+    {
+        num1 += Natural(num1 + 1, num2);
+    }
+    return num1;
+}
