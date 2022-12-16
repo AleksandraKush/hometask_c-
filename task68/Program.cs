e755714f4ee1a71e107b3426f2d5efa73ad678cf
александра <aleksandra@MacBook-Pro-aleksandra.local>
2022-12-16 18:59:19 +0300

    add task68

---
+++ task68/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("введите неотрицательное число M");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("введите неотрицательное число N");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int result = Akkerman(n, m);
+Console.Write(result);
+
+int Akkerman(int number1, int number2)
+{
+    if (number1 == 0)
+    {
+        return number2 + 1;
+    }
+    if (number1 > 0 && number2 == 0)
+    {
+        return Akkerman(number1 - 1, 1);
+    }
+    return Akkerman(number1 - 1, Akkerman(number1, number2 - 1));
+}
